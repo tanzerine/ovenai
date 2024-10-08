65792538afd6406ed1cf0aa0e63287be2a397f41
tanzerine <73976340+tanzerine@users.noreply.github.com>
2024-10-08 16:56:13 +0900

    Update page.tsx

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -4,7 +4,7 @@
     import { useUser } from "@clerk/nextjs"
     import { Button } from "@/components/ui/button"
     import { Input } from "@/components/ui/input"
-    import { Upload as UploadIcon } from 'lucide-react';
+    import { Divide, Upload as UploadIcon } from 'lucide-react';
     import { supabase } from '../../lib/supabase'
     import { useSearchParams } from 'next/navigation';
     import Image from 'next/image';
@@ -33,6 +33,10 @@
       const [isDragging, setIsDragging] = useState(false);
       const fileInputRef = useRef<HTMLInputElement>(null);
     
+    const [isLoading, setIsLoading] = useState(false);
+    const [isImageLoaded, setIsImageLoaded] = useState(false);
+
+
 
 
     const sizeOptions = [
@@ -145,6 +149,8 @@
         }
       
         setIsGenerating(true);
+        setIsLoading(true);
+        setIsImageLoaded(false);
         setError('');
         setOriginalImageUrl(null);
         setRemovedBgImageUrl(null);
@@ -154,11 +160,12 @@
           const newPoints = points - 50;
           setPoints(newPoints); // Update points locally first
       
-          console.log('Sending generate request with prompt:', prompt);
+          console.log('Sending generate request with prompt:', `UNGDUNG ${prompt}`);
           console.log('Image file:', imageFile);
+    
       
           const formData = new FormData();
-          formData.append('prompt', prompt);
+          formData.append('prompt', `UNGDUNG ${prompt}`); // Add the prefix here
           formData.append('size', size);
           if (imageFile) {
             formData.append('image', imageFile);
@@ -173,7 +180,7 @@
           console.log('Received response from generate API:', data);
       
           if (response.ok && data.success && data.predictionId) {
-            await updateUserPoints(newPoints); // Update points in database
+            await updateUserPoints(newPoints);
             console.log('Starting poll for result with predictionId:', data.predictionId);
             await pollForResult(data.predictionId);
           } else {
@@ -183,10 +190,11 @@
           console.error('Error in generateIcon:', err);
           setError(`An error occurred: ${err instanceof Error ? err.message : String(err)}`);
           setPoints(points); // Revert points if there's an error
-        } finally {
+          setIsLoading(false);
           setIsGenerating(false);
         }
       };
+
       const pollForResult = async (predictionId: string) => {
         const maxAttempts = 60 // 60 * 5 seconds = 5 minutes max wait time
         let attempts = 0
@@ -200,7 +208,7 @@
           attempts++
       
           try {
-            console.log(`Checking prediction (Attempt ${attempts}):`, predictionId) // Add this log
+            console.log(`Checking prediction (Attempt ${attempts}):`, predictionId)
       
             if (!predictionId) {
               throw new Error('Prediction ID is missing')
@@ -209,16 +217,27 @@
             const response = await fetch(`/api/check-prediction?id=${predictionId}`)
             const data = await response.json()
       
-            console.log('Received check-prediction response:', data) // Add this log
+            console.log('Received check-prediction response:', data)
       
             if (data.status === 'completed') {
-              setOriginalImageUrl(data.imageUrl)
-              console.log('Image generation completed. URL:', data.imageUrl) // Add this log
+              if (Array.isArray(data.imageUrl) && data.imageUrl.length > 0) {
+                setOriginalImageUrl(data.imageUrl[0]);
+                console.log('Image generation completed. URL:', data.imageUrl[0]);
+              } else if (typeof data.imageUrl === 'string') {
+                setOriginalImageUrl(data.imageUrl);
+                console.log('Image generation completed. URL:', data.imageUrl);
+              } else {
+                setError('Invalid image URL format received from the API');
+                setIsLoading(false);
+                setIsGenerating(false);
+              }
             } else if (data.status === 'failed') {
               setError('Failed to generate icon. Please try again.')
+              setIsLoading(false);
+              setIsGenerating(false);
             } else {
-              console.log('Generation still in progress. Checking again in 5 seconds.') // Add this log
-              setTimeout(checkResult, 5000) // Check again after 5 seconds
+              console.log('Generation still in progress. Checking again in 5 seconds.')
+              setTimeout(checkResult, 5000)
             }
           } catch (error) {
             console.error('Error checking prediction:', error)
@@ -383,21 +402,18 @@
 
   {/* Size Dropdown */}
   <div className="mb-7">
-    <label htmlFor="size" className="text-black text-s font-medium leading-tight block mb-3">Size</label>
-    <div className="relative">
-      <select
-        id="size"
-        value={size}
-        onChange={(e) => setSize(e.target.value)}
-        className="w-full h-10 bg-[#bbbbbb]/20 rounded-[10px] border-[#888888]/10 appearance-none px-3 text-gray-500 text-s border focus:ring-1 focus:ring-blue-500  shadow-sm transition-colors focus:border-blue-500 outline-none"
-        >
-        <option value="" disabled selected hidden>Select size</option>
-        {sizeOptions.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.label}
-          </option>
-        ))}
-      </select>
+        <label htmlFor="size" className="text-black text-s font-medium leading-tight block mb-3">Size</label>
+        <div className="relative">
+          <select
+            id="size"
+            value={size}
+            onChange={(e) => setSize(e.target.value)}
+            className="w-full h-10 bg-[#bbbbbb]/20 rounded-[10px] border-[#888888]/10 appearance-none px-3 text-gray-500 text-s border focus:ring-1 focus:ring-blue-500 shadow-sm transition-colors focus:border-blue-500 outline-none"
+          >
+            <option value="1024">1024</option>
+            <option value="512">512</option>
+            <option value="256">256</option>
+          </select>
       <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-500">
         <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
           <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/>
@@ -412,21 +428,18 @@
 
                 {/* Format Dropdown */}
                 <div className="mb-7">
-                  <label htmlFor="format" className="text-black text-s font-medium leading-tight block mb-3">Format</label>
-                  <div className="relative">
-                    <select
-                      id="format"
-                      value={format}
-                      onChange={(e) => setFormat(e.target.value)}
-                      className="w-full h-10 bg-[#bbbbbb]/20 rounded-[10px] border-[#888888]/10 appearance-none px-3 text-gray-500 text-s border  shadow-sm transition-colors focus:ring-1 focus:ring-blue-500 outline-none"
-                    >
-                      <option value="" disabled selected hidden>Select format</option>
-                      {formatOptions.map((option) => (
-                        <option key={option.value} value={option.value}>
-                          {option.label}
-                        </option>
-                      ))}
-                    </select>
+        <label htmlFor="format" className="text-black text-s font-medium leading-tight block mb-3">Format</label>
+        <div className="relative">
+          <select
+            id="format"
+            value={format}
+            onChange={(e) => setFormat(e.target.value)}
+            className="w-full h-10 bg-[#bbbbbb]/20 rounded-[10px] border-[#888888]/10 appearance-none px-3 text-gray-500 text-s border shadow-sm transition-colors focus:ring-1 focus:ring-blue-500 outline-none"
+          >
+            <option value="PNG">PNG</option>
+            <option value="JPG">JPG</option>
+            <option value="WEBP">WEBP</option>
+          </select>
                     <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-500">
                       <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                         <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/>
@@ -451,48 +464,62 @@
             {/* Divider */}
             <div className="w-full md:w-px h-px md:h-full bg-[#ebebeb] my-5 md:my-0"></div>
 
-            {/* Output Section */}
-            <div className="flex-1 p-5 md:p-[30px] md:py-14 flex flex-col justify-start items-start">
-              <div className="w-full pr-0 md:pr-5">
-                <h5 className="font-clash-grotesk text-[#0c0c0c] text-xl md:text-[26.47px] font-medium leading-tight md:leading-[30.80px] mb-5">Output</h5>
-                <div className="w-full h-px bg-[#ebebeb] mb-5"></div>
-                {error && <p className="text-red-500 text-s font-medium mb-5">{error}</p>}
-                {!originalImageUrl ? (
-                  <p className="text-black/50 text-s font-medium leading-tight mb-5">Press Run to see the results!</p>
-                ) : null}
-                <div className="w-full h-[300px] md:h-[570px] flex justify-center items-center mb-5">
-                  {((showOriginal && originalImageUrl) || (!showOriginal && removedBgImageUrl)) ? (
-                    <Image 
-                    src={showOriginal ? originalImageUrl || '/placeholder.png' : removedBgImageUrl || '/placeholder.png'}
-                    alt="Generated Icon"
-                    width={500} // Set an appropriate width
-                    height={500} // Set an appropriate height
-                    className="max-w-full max-h-full object-contain"
-                  />
-                  ) : (
-                    <div className="w-full h-full bg-gray-200 opacity-20"></div>
-                  )}
-                </div>
-                <div className="w-full pt-[3px] flex flex-col md:flex-row justify-center items-center gap-2.5">
-                  <Button 
-                    className="w-full md:w-[280px] bg-[#333333] text-white font-bold"
-                    onClick={downloadImage}
-                    disabled={!originalImageUrl && !removedBgImageUrl}
-                  >
-                    Download
-                  </Button>
-                  <Button 
-                    className="w-full md:w-[280px] bg-[#e3e3e3] text-[#4a4a4a] font-bold"
-                    onClick={removeBackground}
-                    disabled={!originalImageUrl || isRemovingBackground}
-                  >
-                    {isRemovingBackground ? 'Removing...' : 'Remove Background'}
-                  </Button>
-                </div>
-              </div>
+       {/* Output Section */}
+       <div className="flex-1 p-5 md:p-[30px] md:py-14 flex flex-col justify-start items-start">
+      <div className="w-full pr-0 md:pr-5">
+        <h5 className="font-clash-grotesk text-[#0c0c0c] text-xl md:text-[26.47px] font-medium leading-tight md:leading-[30.80px] mb-5">Output</h5>
+        <div className="w-full h-px bg-[#ebebeb] mb-5"></div>
+        {error && <p className="text-red-500 text-s font-medium mb-5">{error}</p>}
+        {!originalImageUrl && !isLoading && (
+          <p className="text-black/50 text-s font-medium leading-tight mb-5">Press Run to see the results!</p>
+        )}
+        <div className="w-full h-[300px] md:h-[570px] flex justify-center items-center mb-5 bg-[#BBBBBB31] relative overflow-hidden">
+          {isLoading && (
+            <div className="absolute inset-0 flex items-center justify-center">
+              <div className="w-16 h-16 border-4 border-gray-300 border-t-white rounded-full animate-spin"></div>
             </div>
-          </div>
+          )}
+          {((showOriginal && originalImageUrl) || (!showOriginal && removedBgImageUrl)) && (
+            <Image 
+              src={showOriginal ? originalImageUrl! : removedBgImageUrl!}
+              alt="Generated Icon"
+              fill
+              style={{ objectFit: 'contain' }}
+              className={`max-w-full max-h-full transition-opacity duration-300 ${isImageLoaded ? 'opacity-100' : 'opacity-0'}`}
+              onLoadingComplete={() => {
+                setIsImageLoaded(true);
+                setIsLoading(false);
+                setIsGenerating(false);
+              }}
+              onError={() => {
+                setError('Failed to load the image. Please try again.');
+                console.error('Image load error. URL:', showOriginal ? originalImageUrl : removedBgImageUrl);
+                setIsLoading(false);
+                setIsGenerating(false);
+              }}
+            />
+          )}
+        </div>
+        <div className="w-full pt-[3px] flex flex-col md:flex-row justify-center items-center gap-2.5">
+          <Button 
+            className="w-full md:w-[280px] bg-[#333333] text-white font-bold"
+            onClick={downloadImage}
+            disabled={!originalImageUrl && !removedBgImageUrl}
+          >
+            Download
+          </Button>
+          <Button 
+            className="w-full md:w-[280px] bg-[#e3e3e3] text-[#4a4a4a] font-bold"
+            onClick={removeBackground}
+            disabled={!originalImageUrl || isRemovingBackground}
+          >
+            {isRemovingBackground ? 'Removing...' : 'Remove Background'}
+          </Button>
         </div>
       </div>
+    </div>
+    </div>
+    </div>
+</div>
     );
   }
