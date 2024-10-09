d399e165e59e8bacda0ae37fac31854f83de3599
tanzerine <73976340+tanzerine@users.noreply.github.com>
2024-10-09 16:12:56 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,16 +5,6 @@ import { ClerkProvider } from '@clerk/nextjs'
 import MainLayout from '../components/MainLayout'; // Adjust the import path as necessary
 import { Analytics } from "@vercel/analytics/react"
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
