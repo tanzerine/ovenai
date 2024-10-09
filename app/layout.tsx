30943bc49c09d22a7c8af3b77df4666538f1702e
tanzerine <73976340+tanzerine@users.noreply.github.com>
2024-10-09 16:16:59 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout({
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
           <MainLayout>
+            <Analytics />
             {children}
           </MainLayout>
         </body>
